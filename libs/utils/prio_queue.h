7f63efba29193e603d2620c4338b8a56e59a1656
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-05-22 15:58:16 +0200

    prioQ

---
+++ libs/utils/prio_queue.h
@@ -0,0 +1,22 @@
+#include "../web/request.h"
+#include "cJSON.h"
+
+struct discord_prio_packet {
+    int retry_after;
+    enum Request_Type request_type;
+    cJSON *json;
+};
+
+typedef struct prio_node {
+    struct discord_prio_packet *data;
+    prio_node_t *next;
+} prio_node_t;
+
+typedef struct prio_queue {
+    prio_node_t *head;
+};
+
+prio_node_t *prio_push(struct prio_queue *queue, enum Request_Type request_type, cJSON *data, int retry_after);
+prio_node_t *prio_pop(struct prio_queue *queue);
+void prio_destroy_node(prio_node_t *node);
+void prio_destroy_queue(struct prio_queue *queue);
