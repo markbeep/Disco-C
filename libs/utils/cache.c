c0b190052bf6492d51f45ebdab408766c398462d
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-05-31 00:03:09 +0200

    started on cache implementation

---
+++ libs/utils/cache.c
@@ -0,0 +1,14 @@
+#include "cache.h"
+
+static struct hashmap *messages;
+static struct hashmap *channels;
+static struct hashmap *guilds;
+
+uint64_t message_hash() {
+}
+
+void cache_init() {
+}
+
+void cache_set_message(struct discord_message *msg) {
+}
---
+++ libs/utils/cache.h
@@ -0,0 +1,6 @@
+#ifndef DISCO_CACHE
+#define DISCO_CACHE
+
+#include "../discord/structures/message.h"
+
+#endif
